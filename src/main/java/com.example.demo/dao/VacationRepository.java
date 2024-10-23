8b3e648543c33a886ec4121e6c10e9d9dd445731
Markelle Ngishu <mngish1@wgu.edu>
2024-10-23 12:59:22 -0500

    simplify repositories

--- src/main/java/com.example.demo/dao/VacationRepository.java
+++ src/main/java/com.example.demo/dao/VacationRepository.java
@@ -2,10 +2,8 @@ package com.example.demo.dao;
 
 import com.example.demo.entities.Vacation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
-@RepositoryRestResource(collectionResourceRel = "Vacation", path = "Vacation")
 public interface VacationRepository extends JpaRepository<Vacation, Long> {
 }
